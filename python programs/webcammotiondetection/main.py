ac936bcda8215db4b2a679b06df3ece102f34e68
Archiet <archiettanwar71@gmail.com>
2025-08-27 02:27:14 +0530

    checking when the object has entered and when is exiting

--- python programs/webcammotiondetection/main.py
+++ python programs/webcammotiondetection/main.py
@@ -1,13 +1,45 @@
+from emailing import send_email
 import cv2
 import time
 
 video=cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame=None
+status_list=[]
+
 while True:
+    status=0
     check,frame=video.read()
-    cv2.imshow("my video",frame)
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray_frame_blurred=cv2.GaussianBlur(gray_frame,(21,21),0)
     key=cv2.waitKey(1)
+
+    if first_frame is None:
+        first_frame=gray_frame_blurred
+
+    del_frame=cv2.absdiff(first_frame,gray_frame_blurred)
+    thresh_frame=cv2.threshold(del_frame,30,255,cv2.THRESH_BINARY)[1]
+    dil_frame=cv2.dilate(thresh_frame,None,iterations=2)
+    cv2.imshow("my video",dil_frame)
+
+    contours,check=cv2.findContours(dil_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour)<5000:
+            continue
+        x,y,w,h=cv2.boundingRect(contour)
+        rectangle=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,225),3)
+        if rectangle.any():
+            status=1
+
+    status_list.append(status)
+    status_list=status_list[-2:]
+    print(status_list)
+    if status_list[0]==1 and status_list[1]==0:
+        send_email()
+
+    cv2.imshow("Video",frame)
+
     if key==ord("q"):
         break
 
